0e31f5d8434944641254e25b628ae9f411a97212
Denis Tola <denis.tola22@gmail.com>
2021-11-29 22:45:58 +0100

    Add project to VC

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "Wiselabs.ai",
+    subtitle: "Python, Django, PyTorch",
+    description:
+      "WiseLabs.ai is a unique, end-to-end data platform leveraging AI to help you understand how consumers talk about you online.",
+    image: "./project-1.PNG",
+
+  },
+  {
+    title: "Shopper",
+    subtitle: "React and Python",
+    description:
+      "Shopper is a local marketplace for second hand items.",
+    image: "./project-2.PNG",
+
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Team Collaboration Chat inspired from Slack.",
+    image: "./project-3.PNG",
+
+  },
+  {
+    title: "Stealthy",
+    subtitle: "React Native, Python",
+    description:
+      "Mobile chat application with end-to-end encryption enabled.",
+    image: "./project-4.jpg",
+
+  },
+];
+
+export const skills = [
+  "Python",
+  "Django",
+  "Pandas",
+  "JavaScript",
+  "React.js",
+  "Node.js",
+  "React Native",
+  "Postgres",
+  "MongoDB",
+  "Redis",
+  "Docker",
+  "AWS"
+
+];
